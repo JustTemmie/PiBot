72e0f9d41222d1f5e24f117d466c2bd88d521910
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2024-05-28 00:49:50 +0200

    avoid overwriting files with the existing file name

--- cogs/downloader.py
+++ cogs/downloader.py
@@ -12,7 +12,7 @@ class DownloaderCog(commands.Cog):
         
         @bot.tree.context_menu(name="Save Attachments")
         @app_commands.user_install()
-        @discord.is_owner()
+        @commands.is_owner()
         async def download_context_command(interaction: discord.Integration, message: discord.Message) -> None:
             if len(message.attachments) == 0:
                 await interaction.response.send_message("This message doesn't have any attachments", ephemeral=True)
@@ -20,16 +20,46 @@ class DownloaderCog(commands.Cog):
             
             await interaction.response.send_message(f"Oki :3 - downloading {len(message.attachments)} attachment(s)", ephemeral=True)
             
+            folders = {
+                "images": ["png", "svg", "jpeg", "jpg", "webp"],
+                "gifs": ["gif", "gifv"],
+                "videos": ["mp4", "webm", "mvk", "avi", "m4v", "3gp", "mxf", "mov"],
+                "audio": ["ogg", "mp3", "wav", "3gp", "opus", "oga"],
+                "misc": []
+            }
+
+            for folder in folders:
+                media_folder = self.bot.config["FOLDER_PATHS"]["VIDEOS"] + folder
+                if not os.path.exists(media_folder):
+                    os.mkdir(media_folder)
+            
             for attatchment in message.attachments:
                 
-                suffix = ""
+                filename = attatchment.filename
+                folder = f"{list(folders.keys())[-1]}/"
+                prefix = ""
                 new_filename = ""
+                
+                for folder in folders:
+                    for file_type in folders[folder]:
+                        if filename.endswith(file_type):
+                            folder = f"{folder}/"
+                            break
+                    
+                    else:
+                        continue
+                    break
+                
                 while True:
-                    new_filename = self.bot.config["FOLDER_PATHS"]["VIDEOS"] + attatchment.filename + str(suffix)
+                    if prefix == "":
+                        new_filename = self.bot.config["FOLDER_PATHS"]["VIDEOS"] + folder + attatchment.filename
+                    else:
+                        new_filename = self.bot.config["FOLDER_PATHS"]["VIDEOS"] + folder + f"(copy {prefix}) - " + attatchment.filename
+                    
                     if os.path.isfile(new_filename):
-                        if suffix == "":
-                            suffix = 0
-                        suffix += 1
+                        if prefix == "":
+                            prefix = 0
+                        prefix += 1
                     else:
                         break
                 
